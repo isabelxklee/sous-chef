8a36e7ec51bf567c1f627ae4a1d2daffcc01c33e
isabel k. lee <hello@isabelklee.co>
2020-04-16 12:30:37 -0400

    fix all bugs and add CSS styles

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,10 +29,7 @@ class RecipesController < ApplicationController
     end
 
     def update
-        @recipe.update(meal_category: params[:meal_category])
-        # @recipe.cooking_steps.update(cooking_steps_params)
-        # @recipe.ingredients << params[:recipe][:ingredients_attributes]
-        @recipe.update(recipe_params)
+        @recipe.update!(recipe_params)
         redirect_to @recipe
     end
 
@@ -46,11 +43,8 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
     end
 
-    # def cooking_steps_params
-    #     params.require(:cooking_steps).permit(:content)
-    # end
-
     def recipe_params
-        params.require(:recipe).permit(:title, :chef_id, :meal_category, ingredients_attributes: [:id, :name, :quantity, :_destroy], cooking_steps_attributes: [:id, :content, :_destroy])
+        params.require(:recipe).permit(:false, :title, :chef_id, :meal_category, ingredients_attributes: [:id, :name, :quantity, :_destroy], cooking_steps_attributes: [:id, :content, :_destroy])
     end
+
 end
