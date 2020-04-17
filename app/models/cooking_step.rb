74fb49e93df2f094cb1f2d20000d7bbd034428ca
isabel k. lee <hello@isabelklee.co>
2020-04-17 16:20:16 -0400

    created data directory

--- app/models/cooking_step.rb
+++ app/models/cooking_step.rb
@@ -2,5 +2,5 @@ class CookingStep < ApplicationRecord
     belongs_to :recipe
 
     validates :content, length: { maximum: 500 }
-    validates :recipe_id, presence: true
+    # validates :recipe_id, presence: true
 end
